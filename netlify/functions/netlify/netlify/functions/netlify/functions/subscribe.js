77ce6eaea04036728ef5c4f5d148c6f6aefbc618
willworldband <wilberforceosei1@gmail.com>
2026-06-28 06:30:48 +0900

    Create subscribe.js

---
+++ netlify/functions/netlify/netlify/functions/netlify/functions/subscribe.js
@@ -0,0 +1,10 @@
+exports.handler = async (event) => {
+  const headers = {
+    "Access-Control-Allow-Origin":  "*",
+    "Access-Control-Allow-Methods": "POST, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type",
+    "Content-Type": "application/json",
+  };
+  if (event.httpMethod === "OPTIONS") return { statusCode: 200, headers, body: "" };
+  return { statusCode: 200, headers, body: JSON.stringify({ success: true }) };
+};
